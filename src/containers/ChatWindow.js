728036c9011514f366b27328c82545d31f702f76
SaiKumar Immadi <immadisaikumar@ymail.com>
2018-10-22 23:44:40 +0530

    adds chat window showing all chats

--- src/containers/ChatWindow.js
+++ src/containers/ChatWindow.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './ChatWindow.css';
 import { store } from '../store';
+import _ from 'lodash';
 import Header from '../components/Header';
+import Chats from '../components/Chats';
 
 const ChatWindow = ({ activeUserId }) => {
   const state = store.getState();
   const activeUser = state.contacts[activeUserId];
+  const activeMsgs = state.messages[activeUserId];
   return (
     <div className="ChatWindow">
       <Header user={activeUser}/>
+      <Chats messages={_.values(activeMsgs)}/>
     </div>
   );
 };
